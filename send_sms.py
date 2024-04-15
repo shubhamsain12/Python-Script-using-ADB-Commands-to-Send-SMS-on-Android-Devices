ffd76f3fc2f58b075125e68fcd220b36f1086a8b
shubhamsain12 <137056549+shubhamsain12@users.noreply.github.com>
2024-04-15 12:35:38 +0530

    send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -3,10 +3,10 @@ import subprocess
 import time
 
 def send_sms(phone_number, message):  
-    # Escape special characters in the message
+    
     escaped_message = message.replace('$', r'\$').replace('"', r'\"')
 
-    # Format the adb commands to send SMS
+
     adb_commands = [ 
         f'adb shell am start -a android.intent.action.SENDTO -d sms:{phone_number}',
         f'adb shell input text "{escaped_message}"',
@@ -17,18 +17,18 @@ def send_sms(phone_number, message):
         # 'adb shell input keyevent 66',
     ]
 
-    # Execute the adb commands
+
     for cmd in adb_commands:
         try:
             print(f"Executing command: {cmd}")
             subprocess.run(cmd, shell=True, check=True)
-            # Add a delay between commands to ensure proper execution
+         
             time.sleep(4)  # Increased delay to 2 seconds
         except subprocess.CalledProcessError as e:
             print(f"Error executing command: {cmd}")
             print(e)
 
-# Example usage
+
 recipient_number = '+918890066621'  # Replace with the recipient's phone number
 sms_text = 'HELLO'  
 send_sms(recipient_number, sms_text)
